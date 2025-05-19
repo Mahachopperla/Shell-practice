ea37b405de115e35f11d45472825cd8538ecb702
maha <maridayya@gmail.com>
2025-05-19 23:02:31 +0530

    variables usecase

---
+++ 01-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=Maha
+PERSON2=Sathvik
+
+echo " $PERSON1: hii... $PERSON2, how are you "
+echo " $PERSON2: Hello $PERSON1 , I'm good how about you "
+echo " $PERSON1: I'm good thank you!!!"
