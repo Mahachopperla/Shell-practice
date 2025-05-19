b001a776a2a4a240b57102f57e29d2f968892058
maha <maridayya@gmail.com>
2025-05-19 23:25:52 +0530

    variables usecase

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-echo "enter your pin number "
+echo "enter your id number "
 
-read PIN
+read ID
 
+echo " enter your pin: "
+
+read -s PIN # -s hides ur pin visibility from terminal while you are typing
+
+echo "your id is $ID"
 echo "Your pin is $PIN"
+
+
+
