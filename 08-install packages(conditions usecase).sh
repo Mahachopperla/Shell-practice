cc6f050d6a60c53146a06b8a8ab58f9595174e03
maha <maridayya@gmail.com>
2025-05-20 10:56:22 +0530

    installing mysql using conditions

---
+++ 08-install packages(conditions usecase).sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# to install package first we need to check if we are using root access or not
+# installations can only be done with root access
+
+USERID=$(id -u)  # id -u gives userid of current user, root user id will be 0 always
+
+if [ $USERID -ne 0 ]
+then
+    echo " ERROR: please run command with root access to execute succesfully "
+fi
+
+# once logged in as root we need to verify if mysql is already installed or not
+#if already installed no need to install again. if not, proceed to install
+
+dnf list installed mysql
+
+#once we check dnf list installed if mysql is there then exit status will be 0 otherwise it's anything other than 0
+#$? will give exit status of last executed cmnd(in our case it is dnf list)
+
+if [ $? -eq 0 ]  # $? returns 0 if mysql is already there otherwise it is someother num er not 0
+then
+    echo "mysql is already installed... nothing to do"
+else
+    echo "installing MYSQL......."
+    dnf install mysql -y
+    if [ $? -eq 0 ]
+    then
+        echo " installation successfull"
+    else
+        echo " installation failed " 
+    fi
+fi
