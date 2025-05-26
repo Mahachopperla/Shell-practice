0d8f07320895c8be74c01f82cedc991418eb150e
maha <maridayya@gmail.com>
2025-05-26 19:21:51 +0530

    set mechanism testing

--- 13-set.sh
+++ 13-set.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-echo " Hi !! how r u"
-echooo "i'm good"
-echo "how abt u" 
+# Generally basic nature of shell script is that it wont stop it's execution even if in encounters errors
+# example below script:
+
+# echo " Hi !! how r u"
+# echooo "i'm good"
+# echo "how abt u" 
+
+#if we want to stop execution then and there itself when it encounters error use below code
+
+set -e
+
+echo "Hi How are you doing"
+echooo "I'm doing good"
+echo "how abt you"
