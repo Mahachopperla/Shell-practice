0c76e227fd5bc971913fda20c80a6bee354c59f8
maha <maridayya@gmail.com>
2025-05-26 19:33:19 +0530

    set mechanism testing

--- 13-set.sh
+++ 13-set.sh
@@ -9,8 +9,26 @@
 
 #if we want to stop execution then and there itself when it encounters error use below code
 
-set -e
+# set -e
+
+# echo "Hi How are you doing"
+# echooo "I'm doing good"
+# echo "how abt you"
+
+#but the drawback with this is if we are using it in big script we dont know where exactly error occured
+#so set cmnd uses trap bydefalut to trap errors and sent them to failure function
+
+set -e 
+failure(){
+    echo "error occured at : "
+}
+
+trap " failure "$LINENO" "$BASH_COMMENT" ERR "
 
 echo "Hi How are you doing"
 echooo "I'm doing good"
 echo "how abt you"
+
+# Whenver a failure occurs in code it searches for ERR and then goes to trap and will be redirected to failure function
+# for further action
+
