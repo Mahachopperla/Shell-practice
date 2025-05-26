79b6478aec4537876780152bc52f95bf9b6d281c
maha <maridayya@gmail.com>
2025-05-26 20:21:27 +0530

    set mechanism testing

--- 14-delete-old-logs.sh
+++ 14-delete-old-logs.sh
@@ -3,10 +3,17 @@
 Location=/home/ec2-user/Shell-practice/app-logs
 
 files=$(find $Location -name "*.log" -mtime +14)
+echo "deleting logs files older than 14 days: $files"
 
 while IFS= read -r line
 do
-    echo $line
-    
+    rm -rf $line
+    if [ $? -eq 0 ]
+    then 
+        echo "deleted successfully"
+    else
+        echo "deletion failed for $line"
+    fi
+
 done <<< $files
 
