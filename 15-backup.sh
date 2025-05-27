292ac2383ef98354f9819af5ec2e22aa6a994329
maha <maridayya@gmail.com>
2025-05-27 05:48:21 +0530

    set mechanism testing

---
+++ 15-backup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#source dir = /home/ec2-user/Shell-practice/app-logs
+Source_dir=$1
+Dest_dir=$2/backup.zip
+Days="$(3:-14)"
+
+if [ ! -d $Source_dir ] || [ ! -d $Dest_dir ]
+then
+    echo "Please check the args you passed "SOURCE or DESTINATION" directory you specified is not existing"
+    exit 1
+fi
+
+files=$(find $Source_dir -name "*.log" -mtime +14)
+if [ -z "$files" ]; then
+    echo "there are no files which are older than $Days days"
+else
+    echo "zipping logs files older than 14 days: $files"
+
+    while IFS= read -r line
+    do
+        zip -j "$ZIP_FILE" -@ < "$files"
+
+    done <<< $files
+fi
+
+
